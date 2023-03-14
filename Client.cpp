fff3010f1e2326b0d2593e9f5032eda5c6d4eb9f
Kurtis Baker <30036965@students.southwales.ac.uk>
2023-03-14 14:48:47 +0000

    a

---
+++ Client.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  const char* hello = "Hello from client";
+
+  // Create socket
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+    std::cerr << "Socket creation error" << std::endl;
+    return -1;
+  }
+
+  serv_addr.sin_family = AF_INET;
+
+  // Convert IPv4 and IPv6 addresses from text to binary form
+
+  if(inet_pton(AF_INET, "", &serv_addr.sin_addr)<=0)
+    {
+    std::cerr << "Invalid address/ Address not supported" << std::endl;
+    return -1;
+  }
+
+  serv_addr.sin_port = htons( 8080 );
+
+  // Connect to server
+
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+    std::cerr << "Connection Failed" << std::endl;
+    return -1;
+  }
+
+  // Send message to server
+
+  send(sock , hello , strlen(hello) , 0 );
+
+  // Close the socket
+
+  close(sock);
+
+  return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  const char* hello = "Hello from server";
+
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    std::cerr << "socket failed" << std::endl;
+    return -1;
+  }
+
+  // Attach socket to port 8080
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    std::cerr << "setsockopt failed" << std::endl;
+    return -1;
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( 8080 );
+
+  // Bind socket to address
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    std::cerr << "bind failed" << std::endl;
+    return -1;
+  }
+
+  // Listen for incoming connections
+  if (listen(server_fd, 3) < 0) {
+    std::cerr << "listen failed" << std::endl;
+    return -1;
+  }
+
+  // Accept incoming connection
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+    std::cerr << "accept failed" << std::endl;
+    return -1;
+  }
+
+  // Send message to client
+  send(new_socket, hello, strlen(hello), 0);
+  std::cout << "Hello message sent" << std::endl;
+
+  // Close the socket
+  close(server_fd);
+  return 0;
+}
